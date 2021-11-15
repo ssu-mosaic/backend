0a947985e0d155e14794e0c1b68c34bc7bb58b8f
WOOSERK <syeokim@naver.com>
2021-11-15 19:10:37 +0900

    spring

---
+++ src/test/java/kr/co/easystock/BackendApplicationTests.java
@@ -0,0 +1,15 @@
+package kr.co.easystock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
