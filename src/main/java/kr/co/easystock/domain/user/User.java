d3327a84eff91648ab358edf7ca3c1203d839e85
yeojinLee-dev <yeojinlee.dev@gmail.com>
2021-11-25 13:11:27 +0900

    User 도메인 수정

--- src/main/java/kr/co/easystock/domain/user/User.java
+++ src/main/java/kr/co/easystock/domain/user/User.java
@@ -1,7 +1,9 @@
 package kr.co.easystock.domain.user;
 
 import kr.co.easystock.domain.BaseTimeEntity;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
 
@@ -9,18 +11,48 @@ import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @Getter
-@Setter
+@NoArgsConstructor
+//@Setter
 @Entity
+@Table
 public class User extends BaseTimeEntity
 {
+
     @Id
+    @Column
     @GeneratedValue(strategy = GenerationType.IDENTITY) // auto increment
-    private int id;
+    private Long id;
+
+    @Column
+    private String name;
+
+    @Column
     private String businessNo;
+
+    @Column
     private String email;
+
+    @Column
     private String password;
+
+    @Column
     private String address;
+
+    @Column
     private String phone;
+
+    @Column
     private String gender;
+
+    @Builder
+    public User(String name, String password, String businessNo, String email, String address, String phone, String gender) {
+        this.name = name;
+        this.password = password;
+        this.businessNo = businessNo;
+        this.email = email;
+        this.address = address;
+        this.phone = phone;
+        this.gender = gender;
+    }
 
 }
