428457e97ff02dd0bd75c48984728a2a226a3557
WOOSERK <syeokim@naver.com>
2021-12-26 20:42:32 +0900

    로그인 수정

--- src/main/java/kr/co/easystock/service/UserService.java
+++ src/main/java/kr/co/easystock/service/UserService.java
@@ -1,5 +1,6 @@
 package kr.co.easystock.service;
 
+import kr.co.easystock.controller.dto.UserDto;
 import kr.co.easystock.domain.user.User;
 import kr.co.easystock.domain.user.UserRepository;
 import lombok.RequiredArgsConstructor;
@@ -11,8 +12,8 @@ public class UserService
 {
     private final UserRepository userRepository;
 
-    public User login(String name, String password)
+    public User login(UserDto.UserLoginDto userLoginDto)
     {
-        return userRepository.findByNameAndPassword(name, password);
+        return userRepository.findByNameAndPassword(userLoginDto.getName(), userLoginDto.getPassword());
     }
 }
