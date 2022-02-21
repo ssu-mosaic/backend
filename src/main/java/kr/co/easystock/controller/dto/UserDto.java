aa3ad35fb6685df23114164b2e679b0d7fcdb91f
WOOSERK <syeokim@naver.com>
2022-02-21 12:27:03 +0900

    유저Dto 수정

--- src/main/java/kr/co/easystock/controller/dto/UserDto.java
+++ src/main/java/kr/co/easystock/controller/dto/UserDto.java
@@ -9,35 +9,35 @@ public class UserDto
     @Getter
     public static class UserRegisterRequestDto
     {
-        private String id;
-        private String password;
-        private String name;
-        private String businessNo;
-        private String email;
-        private String address;
-        private String phone;
+        private String userId;
+        private String userPwd;
+        private String userName;
+        private String userBusinessNo;
+        private String userEmail;
+        private String userAddress;
+        private String userPhoneNo;
 
         @Builder
-        public UserRegisterRequestDto(String id, String name, String password, String businessNo, String email, String address, String phone)
+        public UserRegisterRequestDto(String userId, String userName, String userPwd, String userBusinessNo, String userEmail, String userAddress, String userPhoneNo)
         {
-            this.id = id;
-            this.name = name;
-            this.password = password;
-            this.businessNo = businessNo;
-            this.email = email;
-            this.address = address;
-            this.phone = phone;
+            this.userId = userId;
+            this.userName = userName;
+            this.userPwd = userPwd;
+            this.userBusinessNo = userBusinessNo;
+            this.userEmail = userEmail;
+            this.userAddress = userAddress;
+            this.userPhoneNo = userPhoneNo;
         }
 
         public User toEntity() {
             return User.builder()
-                    .id(id)
-                    .name(name)
-                    .password(password)
-                    .businessNo(businessNo)
-                    .email(email)
-                    .address(address)
-                    .phone(phone)
+                    .id(userId)
+                    .name(userName)
+                    .password(userPwd)
+                    .businessNo(userBusinessNo)
+                    .email(userEmail)
+                    .address(userAddress)
+                    .phone(userPhoneNo)
                     .build();
         }
     }
