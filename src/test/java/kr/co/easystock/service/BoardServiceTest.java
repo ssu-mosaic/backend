55cdcac7fc6f2d830f9b9628f3115efd83027e08
WOOSERK <syeokim@naver.com>
2022-02-23 12:51:59 +0900

    문의 DTO 수정

--- src/test/java/kr/co/easystock/service/BoardServiceTest.java
+++ src/test/java/kr/co/easystock/service/BoardServiceTest.java
@@ -13,9 +13,6 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -126,7 +123,7 @@ public class BoardServiceTest
 
         // then
         for(InquiryListDto dto : inquiryListDtos)
-            System.out.println(dto.getId() + " : " + dto.getTitle());
+            System.out.println(dto.getInquiryId() + " : " + dto.getInquiryTitle());
 
         Assertions.assertEquals(inquiryListDtos.size(), 20);
     }
